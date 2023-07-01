736d92fa0774c82f57a379162d34ec5b986b24c9
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-01 11:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const submitBtn = document.getElementById("button");
 
 submitBtn.addEventListener("click", (e)=>{
 	e.preventDefault();
+ 
+	e.target.style.borderColor = '#96cbf6';
 	if(name.value && year.value){
 		url.innerText += `?${name.name}=${name.value}&${year.name}=${year.value}`
 	}
-	if(name.value===""){
+	if(name.value==="" && year.value){
 		url.innerText += `?${year.name}=${year.value}`
 	}
-	if(year.value===""){
+	if(year.value==="" && name.value){
 		url.innerText += `?${name.name}=${name.value}`
 	}
-})
+	 
+},{once:true})
