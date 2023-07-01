d9719b62e2874d789f6c9f63a4fea6a6e6f3e245
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-01 10:10:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+let url = document.getElementById("url");
+const name = document.getElementById("name");
+const year = document.getElementById("year");
+const submitBtn = document.getElementById("button");
+
+submitBtn.addEventListener("click", (e)=>{
+	e.preventDefault();
+	if(name.value && year.value){
+		url.innerText += `?${name.name}=${name.value}&${year.name}=${year.value}`
+	}
+	 
+})
